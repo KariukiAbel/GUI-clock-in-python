2ca5e3b5e892b4a200dbca2a825637cb6e56441d
KariukiAbel <abelmuriithi@gmail.com>
2020-06-19 20:35:49 +0300

    GuI clock in python, still learning more python

---
+++ clock.py
@@ -0,0 +1,45 @@
+import sys, types, os
+from time import localtime
+from datetime import timedelta, datetime
+from math import sin, cos, pi
+from threading import Thread
+
+try:
+    from tkinter import *
+except ImportError:
+    try:
+        from mtTkinter import *
+    except ImportError:
+        from Tkinter import *
+        
+hasPIL = True
+try:
+    from PIL import Image, ImageTk
+except ImportError:
+    hasPIL = False
+    
+class mapper:
+    def __init__(self, world, viewport):
+        self.world = world
+        self.viewport = viewport
+        x_min, y_min, x_max, y_max = self.world
+        X_min, Y_min, X_max, Y_max = self.viewport
+        f_x = float(X_max - X_min) / float(x_max - x_min)
+        f_y = float(Y_max - Y_min) / float(y_max - y_min)
+        self.f = min(f_x, f_y)
+        x_c = 0.5 * (x_min + x_max)
+        y_c = 0.5 * (y_min + y_max)
+        X_c = 0.5 * (X_min + X_max)
+        Y_c = 0.5 * (Y_min + Y_max)
+        self.c_1 = X_c - self.f * x_c
+        self.c_2 = Y_c - self.f * y_c
+        
+    
+    def __windowToViewport(self, x, y):
+        X = self.f * x +self.c_1
+        Y = self.f * -y + self.c_2 # Y axis is upside down
+        return X, Y
+
+
+    def windowToViewport(self, x1, y1, x2, y2):
+        return self.__windowToViewport(x1, y2), self.__windowToViewport(x2, y2)        
